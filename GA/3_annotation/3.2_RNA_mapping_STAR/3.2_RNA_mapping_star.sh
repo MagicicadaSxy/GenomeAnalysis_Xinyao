400de19728409f7503341baef81b4ea40ff204a6
MagicicadaSxy <2714485368@qq.com>
2021-05-24 10:14:03 +0200

    modified:   GA/3_annotation/3.2_RNA_mapping_STAR/3.2_RNA_mapping_star.sh

--- GA/3_annotation/3.2_RNA_mapping_STAR/3.2_RNA_mapping_star.sh
+++ GA/3_annotation/3.2_RNA_mapping_STAR/3.2_RNA_mapping_star.sh
@@ -16,18 +16,41 @@ module load star/2.7.2b
 
 
 # create a genome index
-STAR --runThreadN 8 \
---runMode genomeGenerate \
---limitBAMsortRAM 3056146928 \
---genomeChrBinNbits 9 \
---genomeSAindexNbases 12 \
---genomeDir /home/magici/GA/3_annotation/3_2_RNA_mapping_STAR \
---genomeFastaFiles /home/magici/GA/3_annotation/3.1_repeatmasker/hardmask/closed_gaps.fasta.masked \
-
-# read alignment
 STAR --runThreadN 2 \
---genomeDir /home/ziqi/Ziqi_Genome_Analysis/result/7_rna_mapping/index \
---readFilesIn /home/ziqi/Ziqi_Genome_Analysis/2_Eckalbar_2016/sel3/rna_seq_data/trimmed/*.fastq.gz \
---outFileNamePrefix /home/ziqi/Ziqi_Genome_Analysis/result/7_rna_mapping/star/Mnat \
---outSAMtype BAM SortedByCoordinate \
---readFilesCommand zcat
+--runMode genomeGenerate \
+--genomeChrBinNbits 18 \
+--genomeSAindexNbases 9 \
+--genomeDir /home/magici/GA/3_annotation/3.2_RNA_mapping_STAR/index \
+--genomeFastaFiles /home/magici/GA/sel3_NW_015504249.fna \
+
+# reads alignment
+
+for num in 013 014 015 016 017 018 204 206 207 208 209 211 212 213 214 241 242
+	do
+		file1=/home/magici/GA/data/sel3/rna_seq_data/trimmed/sel3_SRR1719"$num".trim_1P.fastq.gz
+		file2=/home/magici/GA/data/sel3/rna_seq_data/trimmed/sel3_SRR1719"$num".trim_2P.fastq.gz		
+		
+
+		STAR --runThreadN 2 \
+                     --genomeDir /home/magici/GA/3_annotation/3.2_RNA_mapping_STAR/index \
+                     --readFilesIn $file1 $file2 \
+                     --outFileNamePrefix /home/magici/GA/3_annotation/3.2_RNA_mapping_STAR/"$num"P_ \
+                     --outSAMtype BAM SortedByCoordinate \
+                     --readFilesCommand zcat
+
+
+
+		file3=/home/magici/GA/data/sel3/rna_seq_data/trimmed/sel3_SRR1719"$num".trim_1U.fastq.gz
+                file4=/home/magici/GA/data/sel3/rna_seq_data/trimmed/sel3_SRR1719"$num".trim_2U.fastq.gz
+
+		echo 
+                STAR --runThreadN 2 \
+                     --genomeDir /home/magici/GA/3_annotation/3.2_RNA_mapping_STAR/index \
+                     --readFilesIn $file3 $file4 \
+                     --outFileNamePrefix /home/magici/GA/3_annotation/3.2_RNA_mapping_STAR/"$num"U_ \
+                     --outSAMtype BAM SortedByCoordinate \
+                     --readFilesCommand zcat
+
+
+	done
+	
